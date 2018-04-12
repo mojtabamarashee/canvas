221e8ac26b19d837ea0180ef0f83b97049881c3d
dsp <you@example.com>
2018-04-12 15:17:23 -0700

    add child

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 		<CanvasCom>
-			<Axis />	
+			<Axis name="mojtaba"/>	
 		</CanvasCom>
       </div>
     );
--- src/com.js
+++ src/com.js
@@ -1,4 +1,5 @@
 
+import ReactDOM from 'react-dom'
 import React, { Component } from 'react';
 
 export class Axis extends Component {
@@ -22,8 +23,32 @@ export class CanvasCom extends React.Component {
     updateCanvas() {
         const ctx = this.refs.canvas.getContext('2d');
         ctx.fillRect(0,0, 100, 100);
+
+
+		{React.Children.map(this.props.children, (child, i) => {
+		  // Ignore the first child
+			console.log(child.props.name)
+		})}
+
+
+
+
+
     }
+
+
+
+
+
+
+
     render() {
+
+
+
+
+
+
         return (
             <canvas ref="canvas" width={300} height={300}/>
         );
